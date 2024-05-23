9981b114135a72934f0265e95c461b72047844a2
Pipo <156736428+pannathatdev@users.noreply.github.com>
2024-05-23 12:39:53 +0700

    Update mockup-loader.js

--- order-service/src/order/mockup-loader.js
+++ order-service/src/order/mockup-loader.js
@@ -1,20 +1,16 @@
 const axios = require('axios');
-const path = require('path');
-const fs = require('fs');
-
-// ระบุเส้นทางสมบูรณ์ของไฟล์ mock-products.json
-const mockProductsPath = path.join(__dirname, 'mock-products.json');
-
-// โหลดข้อมูลจากไฟล์ mock-products.json
-const mockProductsData = JSON.parse(fs.readFileSync(mockProductsPath, 'utf-8'));
-
-// โหลดข้อมูล orders จากไฟล์ mock-orders.json โดยอยู่ในโฟลเดอร์เดียวกัน
-const orders = require('./mock-orders.json');
 
 async function loadMockupData() {
     try {
+        // ใช้ URL ของ Raw ของไฟล์ mock-products.json ใน GitHub repository "product-service"
+        const productsUrl = 'https://raw.githubusercontent.com/pannathatdev/product-service/main/src/mock-products.json';
+
+        // ดึงข้อมูลจาก URL ด้วย axios
+        const response = await axios.get(productsUrl);
+        const products = response.data;
+
         // เรียกใช้ service ผ่านชื่อ container ที่กำหนดใน docker-compose.yml
-        await axios.post('http://order-service:9000/orders/load', orders);
+        await axios.post('http://order-service:9000/orders/load', products);
         console.log('Mockup data loaded successfully.');
     } catch (error) {
         console.error('Failed to load mockup data:', error.message);
