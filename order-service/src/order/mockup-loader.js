24dfc1a321caacb32fd7152ac2135c4370f2cbc8
Pipo <156736428+pannathatdev@users.noreply.github.com>
2024-05-24 15:20:25 +0700

    แก้ไขการอัพข้อมูล

--- order-service/src/order/mockup-loader.js
+++ order-service/src/order/mockup-loader.js
@@ -2,15 +2,11 @@ const axios = require('axios');
 
 async function loadMockupData() {
     try {
-        // ใช้ URL ของ Raw ของไฟล์ mock-products.json ใน GitHub repository "product-service"
         const productsUrl = 'https://raw.githubusercontent.com/pannathatdev/product-service/main/src/mock-products.json';
-
-        // ดึงข้อมูลจาก URL ด้วย axios
         const response = await axios.get(productsUrl);
         const products = response.data;
 
-        // เรียกใช้ service ผ่านชื่อ container ที่กำหนดใน docker-compose.yml
-        await axios.post('http://host.docker.internal:9000', products);
+        await axios.post('http://order-service:9000/orders/load', products);
         console.log('Mockup data loaded successfully.');
     } catch (error) {
         console.error('Failed to load mockup data:', error.message);
--- order-service/src/order/order.controller.ts
+++ order-service/src/order/order.controller.ts
@@ -7,7 +7,6 @@ export class OrderController {
 
   @Get()
   async findAllOrders(): Promise<any> {
-    // เรียกใช้งาน endpoint ของ product-service เพื่อขอข้อมูลสินค้า
     try {
       const response = await axios.get('http://product-service:8000/products');
       return response.data;
@@ -19,10 +18,8 @@ export class OrderController {
 
   @Post('load')
   async loadProducts(@Body() products: any) {
-    // ทำการโหลดข้อมูลสินค้าและทำการประมวลผลต่อไปตามต้องการ
     try {
       console.log('Received products:', products);
-      // ทำการประมวลผลต่อไปตามต้องการ เช่น บันทึกข้อมูลลงฐานข้อมูล
       return { message: 'Products loaded successfully' };
     } catch (error) {
       console.error('Failed to process products:', error.message);
