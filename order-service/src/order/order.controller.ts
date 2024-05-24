0e83388e4f41f9283b48c2e45b5a596f22d9ecc3
Pipo <156736428+pannathatdev@users.noreply.github.com>
2024-05-24 11:57:43 +0700

    Update order.controller.ts

--- order-service/src/order/order.controller.ts
+++ order-service/src/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import axios from 'axios';
 
 @Controller('orders')
@@ -9,10 +9,23 @@ export class OrderController {
   async findAllOrders(): Promise<any> {
     // เรียกใช้งาน endpoint ของ product-service เพื่อขอข้อมูลสินค้า
     try {
-        const response = await axios.get('http://product-service:8000/products');
+      const response = await axios.get('http://product-service:8000/products');
       return response.data;
     } catch (error) {
       console.error('Failed to fetch products:', error.message);
+      throw error;
+    }
+  }
+
+  @Post('load')
+  async loadProducts(@Body() products: any) {
+    // ทำการโหลดข้อมูลสินค้าและทำการประมวลผลต่อไปตามต้องการ
+    try {
+      console.log('Received products:', products);
+      // ทำการประมวลผลต่อไปตามต้องการ เช่น บันทึกข้อมูลลงฐานข้อมูล
+      return { message: 'Products loaded successfully' };
+    } catch (error) {
+      console.error('Failed to process products:', error.message);
       throw error;
     }
   }
